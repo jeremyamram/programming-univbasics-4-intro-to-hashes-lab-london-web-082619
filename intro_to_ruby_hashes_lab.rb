58471548a402365d1b363bcbcc9c3e731296eb47
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-22 15:25:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,11 +20,12 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  
+  {key: "Adam", value: "10"}
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  {hash: "hash", key: "key"}
 end
 
 def update_counting_hash(hash, key)
